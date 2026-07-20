05f6627ddf20e4de47cdc00e79fa15e67ba01f4b
AbsoluteVortex <grecolagecko@gmail.com>
2026-07-20 14:34:09 -0400

    Add search functionality with logging to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+console.log("NFL Analytics Hub Loaded");
+
+const search = document.getElementById("searchBar");
+
+search.addEventListener("keyup", function(){
+
+    console.log("Searching for:", search.value);
+
+});
 
